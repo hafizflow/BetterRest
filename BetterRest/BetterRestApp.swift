ee3d496855c4055a0acf5ba3b1c0e9fad0420c5f
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-11-05 15:23:22 +0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Hafizur Rahman on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
