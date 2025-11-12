b9e6d0e402a554b68088012c7c20ac3af6e986c7
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-11-12 19:26:31 +0600

    BetterRest
    A CoreML project intend to guess the user sleep time.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,17 +5,81 @@
 //  Created by Hafizur Rahman on 5/11/25.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUpTime = defaultWakeUpTime
+    @State private var sleepHours = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var isAlertVisible = false
+    
+    static var defaultWakeUpTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?").font(.headline)
+                    
+                    DatePicker("Pick a time", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep").font(.headline)
+                    
+                    Stepper("\(sleepHours.formatted())",value: $sleepHours, in: 4...12, step: 0.25)
+                }
+                    
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake").font(.headline)
+                    
+                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20, step: 1)
+                }
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button ("Calculate", action: calculateBedTime)
+            }
+            .alert(alertTitle, isPresented: $isAlertVisible) {
+                Button("Cancle") {}
+            }
+            message: {
+                Text(alertMessage)
+            }
         }
-        .padding()
+    }
+    
+    
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUpTime)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepHours, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUpTime - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is...."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Failed to calculate bedtime."
+        }
+        
+        isAlertVisible = true
     }
 }
 
