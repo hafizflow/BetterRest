0cfb21f6cb436191ff5567d976ff97ab65b72afe
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-11-12 21:46:56 +0600

    Picker, Remove cal button and many more

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -1,9 +1,9 @@
-//
-//  ContentView.swift
-//  BetterRest
-//
-//  Created by Hafizur Rahman on 5/11/25.
-//
+    //
+    //  ContentView.swift
+    //  BetterRest
+    //
+    //  Created by Hafizur Rahman on 5/11/25.
+    //
 
 import CoreML
 import SwiftUI
@@ -13,9 +13,8 @@ struct ContentView: View {
     @State private var sleepHours = 8.0
     @State private var coffeeAmount = 1
     
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
     @State private var isAlertVisible = false
+    @State private var isExpanded = true
     
     static var defaultWakeUpTime: Date {
         var components = DateComponents()
@@ -24,43 +23,51 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? Date.now
     }
     
+    var recommendedBedtime: String {
+        calculateBedTime()
+    }
+    
     var body: some View {
         NavigationStack {
+            
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?").font(.headline)
-                    
+                DisclosureGroup(isExpanded: $isExpanded) {
                     DatePicker("Pick a time", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
                         .labelsHidden()
+                } label: {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
+                Section("Desired amount of sleep", isExpanded: $isExpanded, content: {
                     Text("Desired amount of sleep").font(.headline)
                     
                     Stepper("\(sleepHours.formatted())",value: $sleepHours, in: 4...12, step: 0.25)
+                } )
+                
+                
+                
+                Picker("Daily coffee intake", selection: $coffeeAmount) {
+                    ForEach(1...20, id: \.self) { number in
+                        Text("^[\(number) cup](inflect: true)")
+                    }
+                    
                 }
-                    
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake").font(.headline)
-                    
-                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20, step: 1)
+                .customVStack()
+                
+                Section("Your ideal bedtime") {
+                    Text(recommendedBedtime)
+                        .font(.system(size: 36, weight: .bold, design: .rounded))
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.vertical)
                 }
             }
             .navigationTitle("BetterRest")
-            .toolbar {
-                Button ("Calculate", action: calculateBedTime)
-            }
-            .alert(alertTitle, isPresented: $isAlertVisible) {
-                Button("Cancle") {}
-            }
-            message: {
-                Text(alertMessage)
-            }
         }
     }
     
     
-    func calculateBedTime() {
+    func calculateBedTime() -> String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -72,17 +79,28 @@ struct ContentView: View {
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepHours, coffee: Double(coffeeAmount))
             
             let sleepTime = wakeUpTime - prediction.actualSleep
-            alertTitle = "Your ideal bedtime is...."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            return sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Failed to calculate bedtime."
+            return "Faild to calculate"
         }
-        
-        isAlertVisible = true
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+struct CustomVStack: ViewModifier {
+    func body(content: Content) -> some View {
+        VStack(alignment: .leading, spacing: 0) {
+            content
+        }
+    }
+}
+
+extension View {
+    func customVStack() -> some View {
+        self.modifier(CustomVStack())
+    }
 }
