a80773d7c14787ba40b2bef0089641ec076ac0dc
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2026-01-07 23:51:54 +0600

    Complete

--- BetterRest/Practice.swift
+++ BetterRest/Practice.swift
@@ -1,19 +1,59 @@
-//
-//  Practice.swift
-//  BetterRest
-//
-//  Created by Hafizur Rahman on 20/11/25.
-//
-
 import SwiftUI
 
+@Observable
+class Info {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
 struct Practice: View {
+    @State private var user = Info(name: "hafiz", age: 23)
+    
     var body: some View {
-        ForEach(1..<5) {
-            Text("Hello World \($0)")
+        TabView {
+            Tab("Home", systemImage: "house") {
+                Home(user: user)
+            }
+            
+            Tab("House", systemImage: "house") {
+                House(user: user)
+            }
         }
     }
 }
+
+struct Home: View {
+    @Bindable var user: Info     // Editable
+    
+    var body: some View {
+        VStack {
+            Text("Home Screen")
+            Text("Name: \(user.name)")
+            
+            TextField("Enter Name...", text: $user.name)   // Editable
+                .textFieldStyle(.roundedBorder)
+        }
+        .padding()
+    }
+}
+
+struct House: View {
+    var user: Info    // ❗ Not Bindable → Read-Only
+    
+    var body: some View {
+        VStack {
+            Text("House Screen (Read Only)")
+            Text("Name: \(user.name)")   // Shows updated value
+        }
+        .padding()
+    }
+}
+
 
 #Preview {
     Practice()
